6d781095f7ed060df5c991de0f4b2eaf2df30fc9
r0s <r0s@tosh.(none)>
2012-05-10 20:31:07 +0200

    Advancing in the project

---
+++ src/delta_construct.py
@@ -0,0 +1,229 @@
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+from geometry_msgs.msg import Point
+
+def create_delta_simulation():
+    print "Creating simulation"
+    markerArray = MarkerArray()
+    width = 0.005
+    
+    
+    # Point of first dynamixel
+    base1 = Marker()
+    base1.header.frame_id = "/map"
+    base1.type = base1.SPHERE
+    base1.action = base1.ADD
+    base1.scale.x = width
+    base1.scale.y = width
+    base1.scale.z = width
+    base1.color.a = 1.0
+    base1.color.r = 1.0
+    base1.color.g = 0.0
+    base1.color.b = 0.0
+    base1.pose.orientation.w = 1.0
+    base1.pose.position.x = 0.051961524227
+    base1.pose.position.y = 0
+    base1.pose.position.z = 0
+    base1.id = 1
+    
+    # Point of second dynamixel
+    base2 = Marker()
+    base2.header.frame_id = "/map"
+    base2.type = base2.SPHERE
+    base2.action = base2.ADD
+    base2.scale.x = width
+    base2.scale.y = width
+    base2.scale.z = width
+    base2.color.a = 1.0
+    base2.color.r = 0.0
+    base2.color.g = 1.0
+    base2.color.b = 0.0
+    base2.pose.orientation.w = 1.0
+    base2.pose.position.x = -0.051961524227
+    base2.pose.position.y = -0.06
+    base2.pose.position.z = 0
+    base2.id = 2
+    
+    # Point of third dynamixel
+    base3 = Marker()
+    base3.header.frame_id = "/map"
+    base3.type = base3.SPHERE
+    base3.action = base3.ADD
+    base3.scale.x = width
+    base3.scale.y = width
+    base3.scale.z = width
+    base3.color.a = 1.0
+    base3.color.r = 0.0
+    base3.color.g = 0.0
+    base3.color.b = 1.0
+    base3.pose.orientation.w = 1.0
+    base3.pose.position.x = -0.051961524227
+    base3.pose.position.y = 0.06
+    base3.pose.position.z = 0
+    base3.id = 3
+    
+    
+    
+    
+    # Arrow from first dynamixel to elbow
+    arrow1 = Marker()
+    arrow1.header.frame_id = "/map"
+    arrow1.type = arrow1.ARROW
+    arrow1.action = arrow1.ADD
+    arrow1.scale.x = width
+    arrow1.scale.y = width
+    arrow1.color.a = 1.0
+    arrow1.color.r = 1.0
+    arrow1.color.g = 0.0
+    arrow1.color.b = 0.0
+    arrow1.points = [None]*2
+    arrow1.points[0] = Point(0.051961524227, 0.0, 0.0)
+    arrow1.points[1] = Point(0.051961524227, 0.0, 0.08)
+    arrow1.pose.orientation.w = 1.0
+    arrow1.id = 4
+    
+    
+    # Arrow from second dynamixel to elbow
+    arrow2 = Marker()
+    arrow2.header.frame_id = "/map"
+    arrow2.type = arrow2.ARROW
+    arrow2.action = arrow2.ADD
+    arrow2.scale.x = width
+    arrow2.scale.y = width
+    arrow2.color.a = 1.0
+    arrow2.color.r = 0.0
+    arrow2.color.g = 1.0
+    arrow2.color.b = 0.0
+    arrow2.points = [None]*2
+    arrow2.points[0] = Point(-0.051961524227, -0.06, 0.0)
+    arrow2.points[1] = Point(-0.051961524227, -0.06, 0.08)
+    arrow2.pose.orientation.w = 1.0
+    arrow2.id = 5
+    
+    # Arrow from third dynamixel to elbow
+    arrow3 = Marker()
+    arrow3.header.frame_id = "/map"
+    arrow3.type = arrow3.ARROW
+    arrow3.action = arrow3.ADD
+    arrow3.scale.x = width
+    arrow3.scale.y = width
+    arrow3.color.a = 1.0
+    arrow3.color.r = 0.0
+    arrow3.color.g = 0.0
+    arrow3.color.b = 1.0
+    arrow3.points = [None]*2
+    arrow3.points[0] = Point(-0.051961524227, 0.06, 0.0)
+    arrow3.points[1] = Point(-0.051961524227, 0.06, 0.08)
+    arrow3.pose.orientation.w = 1.0
+    arrow3.id = 6
+    
+    # Arrow from  elbow to final for first dynamixel
+    arrow4 = Marker()
+    arrow4.header.frame_id = "/map"
+    arrow4.type = arrow4.ARROW
+    arrow4.action = arrow4.ADD
+    arrow4.scale.x = width
+    arrow4.scale.y = width
+    arrow4.color.a = 1.0
+    arrow4.color.r = 1.0
+    arrow4.color.g = 0.0
+    arrow4.color.b = 0.0
+    arrow4.points = [None]*2
+    arrow4.points[0] = Point(0.051961524227, 0.0, 0.08)
+    arrow4.points[1] = Point(0.051961524227, 0.0, 0.2)
+    arrow4.pose.orientation.w = 1.0
+    arrow4.id = 7
+    
+    # Arrow from second dynamixel to elbow
+    arrow5 = Marker()
+    arrow5.header.frame_id = "/map"
+    arrow5.type = arrow5.ARROW
+    arrow5.action = arrow5.ADD
+    arrow5.scale.x = width
+    arrow5.scale.y = width
+    arrow5.color.a = 1.0
+    arrow5.color.r = 0.0
+    arrow5.color.g = 1.0
+    arrow5.color.b = 0.0
+    arrow5.points = [None]*2
+    arrow5.points[0] = Point(-0.051961524227, -0.06, 0.08)
+    arrow5.points[1] = Point(-0.051961524227, -0.06, 0.2)
+    arrow5.pose.orientation.w = 1.0
+    arrow5.id = 8
+    
+    # Arrow from third dynamixel to elbow
+    arrow6 = Marker()
+    arrow6.header.frame_id = "/map"
+    arrow6.type = arrow6.ARROW
+    arrow6.action = arrow6.ADD
+    arrow6.scale.x = width
+    arrow6.scale.y = width
+    arrow6.color.a = 1.0
+    arrow6.color.r = 0.0
+    arrow6.color.g = 0.0
+    arrow6.color.b = 1.0
+    arrow6.points = [None]*2
+    arrow6.points[0] = Point(-0.051961524227, 0.06, 0.08)
+    arrow6.points[1] = Point(-0.051961524227, 0.06, 0.2)
+    arrow6.pose.orientation.w = 1.0
+    arrow6.id = 9
+    
+    
+    
+    
+    floor = Marker()
+    floor.header.frame_id = "/map"
+    floor.type = floor.CUBE
+    floor.action = floor.ADD
+    floor.scale.x = .2
+    floor.scale.y = .2
+    floor.scale.z = width
+    floor.color.a = 1.0
+    # brown: 205-133-63
+    floor.color.r = 205.0 / 255.0
+    floor.color.g = 133.0 / 255.0
+    floor.color.b = 63.0 / 255.0
+    floor.pose.orientation.w = 1.0
+    floor.pose.position.x = 0
+    floor.pose.position.y = 0
+    floor.pose.position.z = -width
+    floor.id = 10
+    
+    
+    # Append marker
+    markerArray.markers.append(base1)
+    markerArray.markers.append(base2)
+    markerArray.markers.append(base3)
+    markerArray.markers.append(arrow1)
+    markerArray.markers.append(arrow2)
+    markerArray.markers.append(arrow3)
+    markerArray.markers.append(arrow4)
+    markerArray.markers.append(arrow5)
+    markerArray.markers.append(arrow6)
+    markerArray.markers.append(floor)
+    
+    return markerArray
+
+def set_initial_delta_pose(markerArray):
+    print "Setting initial pose"
+    endpoint = Point(0.0, 0.0, 0.150286912)
+    for m in markerArray.markers:
+        if m.id == 4: # from dyn1 to elbow1
+            m.points[1] = Point(0.051961524227 + 0.08, 0.0, 0.0)
+        elif m.id == 5: # from dyn2 to elbow2
+            m.points[1] = Point(-0.051961524227 - 0.056568542, -0.06 - 0.056568542, 0.0)
+        elif m.id == 6: # from dyn3 to elbow3
+            m.points[1] = Point(-0.051961524227 - 0.056568542, 0.06 + 0.056568542, 0.0)
+        elif m.id == 7: # from elbow1 to end
+            m.points[0] = Point(0.051961524227 + 0.08, 0.0, 0.0)
+            m.points[1] = endpoint
+        elif m.id == 8: # from elbow2 to end
+            m.points[0] = Point(-0.051961524227 - 0.056568542, -0.06 - 0.056568542, 0.0)
+            m.points[1] = endpoint
+        elif m.id == 9: # from elbow3 to end
+            m.points[0] = Point(-0.051961524227 - 0.056568542, 0.06 + 0.056568542, 0.0)
+            m.points[1] = endpoint
+    return markerArray  
+        
+        
+    
---
+++ src/delta_kinematics.py
@@ -0,0 +1,100 @@
+import math
+# robot geometry
+e = 0.0    # end effector radius
+f = 240.0   # base radius (2x dynamixel to dynamixel distance)
+re = 200.0   # end to elbow dist
+rf = 80.0   # base to elbow dist
+ 
+# trigonometric constants
+sqrt3 = math.sqrt(3.0)
+pi = 3.141592653
+sin120 = sqrt3/2.0
+cos120 = -0.5    
+tan60 = sqrt3
+sin30 = 0.5
+tan30 = 1/sqrt3
+ 
+# forward kinematics: (theta1, theta2, theta3) -> (x0, y0, z0)
+# returned status: 0=OK, -1=non-existing position
+def delta_calcForward(theta1, theta2, theta3):
+    t = (f-e)*tan30/2
+    dtr = pi/180.0
+    
+    theta1 *= dtr
+    theta2 *= dtr
+    theta3 *= dtr
+    
+    y1 = -(t + rf*math.cos(theta1))
+    z1 = -rf*math.sin(theta1)
+    
+    y2 = (t + rf*math.cos(theta2))*sin30
+    x2 = y2*tan60
+    z2 = -rf*math.sin(theta2)
+    
+    y3 = (t + rf*math.cos(theta3))*sin30
+    x3 = -y3*tan60
+    z3 = -rf*math.sin(theta3)
+    
+    dnm = (y2-y1)*x3-(y3-y1)*x2
+    
+    w1 = y1*y1 + z1*z1
+    w2 = x2*x2 + y2*y2 + z2*z2
+    w3 = x3*x3 + y3*y3 + z3*z3
+    
+    # x = (a1*z + b1)/dnm
+    a1 = (z2-z1)*(y3-y1)-(z3-z1)*(y2-y1)
+    b1 = -((w2-w1)*(y3-y1)-(w3-w1)*(y2-y1))/2.0
+    
+    # y = (a2*z + b2)/dnm;
+    a2 = -(z2-z1)*x3+(z3-z1)*x2
+    b2 = ((w2-w1)*x3 - (w3-w1)*x2)/2.0
+    
+    # a*z^2 + b*z + c = 0
+    a = a1*a1 + a2*a2 + dnm*dnm
+    b = 2*(a1*b1 + a2*(b2-y1*dnm) - z1*dnm*dnm)
+    c = (b2-y1*dnm)*(b2-y1*dnm) + b1*b1 + dnm*dnm*(z1*z1 - re*re)
+    
+    # discriminant
+    d = b*b - 4.0*a*c
+    if d < 0:
+        return (-1, 0, 0 ,0) # non-existing point
+    
+    z0 = -0.5*(b+math.sqrt(d))/a
+    x0 = (a1*z0 + b1)/dnm
+    y0 = (a2*z0 + b2)/dnm
+    return (0, x0, y0, z0)
+ 
+ 
+ # inverse kinematics
+ # helper functions, calculates angle theta1 (for YZ-pane)
+def delta_calcAngleYZ(x0, y0, z0):
+    y1 = -0.5 * 0.57735 * f # f/2 * tg 30
+    y0 -= 0.5 * 0.57735 * e # shift center to edge
+    # z = a + b*y
+    a = (x0*x0 + y0*y0 + z0*z0 +rf*rf - re*re - y1*y1)/(2*z0)
+    b = (y1-y0)/z0
+    # discriminant
+    d = -(a+b*y1)*(a+b*y1)+rf*(b*b*rf+rf)
+    if d < 0:
+        return (-1, 0) # non-existing point
+    yj = (y1 - a*b - math.sqrt(d))/(b*b + 1) # choosing outer point
+    zj = a + b*yj
+    theta = 180.0*math.atan(-zj/(y1 - yj))/pi + 180.0 if (yj>y1) else 0.0
+    return (0, theta)
+ 
+ 
+# inverse kinematics: (x0, y0, z0) -> (theta1, theta2, theta3)
+# returned status: 0=OK, -1=non-existing position
+def delta_calcInverse(x0, y0, z0):
+     theta1 = theta2 = theta3 = 0.0
+     status = delta_calcAngleYZ(x0, y0, z0)
+     if status[0] == 0: 
+         status = delta_calcAngleYZ(x0*cos120 + y0*sin120, y0*cos120-x0*sin120, z0)   #rotate coords to +120 deg
+         if status[0] == 0:
+                theta2 = status[1]
+     if status[0] == 0: 
+         status = delta_calcAngleYZ(x0*cos120 - y0*sin120, y0*cos120+x0*sin120, z0)   #rotate coords to -120 deg
+         if status[0] == 0:
+                theta3 = status[1]
+     return (status[0], theta1, theta2, theta3)
+ 
