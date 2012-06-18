8648ba725866d3b6a72bc345e0640c8684fd6274
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2012-06-18 03:19:59 +0200

    fixed a bug with the points of the trail

--- src/delta_passive_move.py
+++ src/delta_passive_move.py
@@ -20,11 +20,15 @@ motor2_pos = 0
 global motor3_pos
 motor3_pos = 0
 global initializedMarkerArray
+global trailMarkerArray
+trailMarkerArray = MarkerArray()
 
 global max_trail_points
 max_trail_points = 100
 global current_trail_point
 current_trail_point = 600
+global current_trail_point_qtty
+current_trail_point_qtty = 0
 
 def generate_last_visited_point(x, y, z):
     global max_trail_points
@@ -47,9 +51,9 @@ def generate_last_visited_point(x, y, z):
     testpoint.pose.position.y = y
     testpoint.pose.position.z = z
     testpoint.id = current_trail_point
-    current_trail_point += 1
     if current_trail_point - 600 >= max_trail_points:
-        current_trail_point = 600
+        current_trail_point = 601
+    current_trail_point += 1
     return testpoint
 
 
@@ -58,39 +62,66 @@ def callback_1(data):
     global motor2_pos
     global motor3_pos
     global initializedMarkerArray
+    global trailMarkerArray
+    global current_trail_point
+    global current_trail_point_qtty
+    global max_trail_points
     # we move from 500 to 850 so we need to adapt the data (1023 is max pos)
     motor1_pos = math.degrees(data.current_pos)#-500.0)/ 350.0 * 90.0
     #print "Motor1 in pos: %d" % (motor1_pos)
     posefinal = delta_calcForward(motor1_pos, motor2_pos, motor3_pos)
     initializedMarkerArray = move_simulation_to_point(posefinal[1], posefinal[2], posefinal[3], initializedMarkerArray)
     point = generate_last_visited_point(posefinal[1], posefinal[2], posefinal[3])
-    initializedMarkerArray.markers.append(point)
+    current_trail_point_qtty += 1
+    if current_trail_point_qtty >= max_trail_points:
+        trailMarkerArray.markers.pop(0) # the 11th element is the first testpoint always
+        current_trail_point_qtty -= 1
+    trailMarkerArray.markers.append(point)
+    #publisher.publish(trailMarkerArray)
     
 def callback_2(data):
     global motor1_pos
     global motor2_pos
     global motor3_pos
     global initializedMarkerArray
+    global trailMarkerArray
+    global current_trail_point
+    global current_trail_point_qtty
+    global max_trail_points
     # we move from 500 to 850 so we need to adapt the data (1023 is max pos)
     motor2_pos = math.degrees(data.current_pos)#(data.position-500.0)/ 350.0 * 90.0
     #print "Motor2 in pos: %d" % (motor2_pos)
     posefinal = delta_calcForward(motor1_pos, motor2_pos, motor3_pos)
     initializedMarkerArray = move_simulation_to_point(posefinal[1], posefinal[2], posefinal[3], initializedMarkerArray)
     point = generate_last_visited_point(posefinal[1], posefinal[2], posefinal[3])
-    initializedMarkerArray.markers.append(point)
+    current_trail_point_qtty += 1
+    if current_trail_point_qtty >= max_trail_points:
+        trailMarkerArray.markers.pop(0) # the 11th element is the first testpoint always
+        current_trail_point_qtty -= 1
+    trailMarkerArray.markers.append(point)
+    #publisher.publish(trailMarkerArray)
     
 def callback_3(data):
     global motor1_pos
     global motor2_pos
     global motor3_pos
     global initializedMarkerArray
+    global trailMarkerArray
+    global current_trail_point
+    global current_trail_point_qtty
+    global max_trail_points
     # we move from 500 to 850 so we need to adapt the data (1023 is max pos)
     motor3_pos = math.degrees(data.current_pos)#(data.position-500.0)/ 350.0 * 90.0
     #print "Motor3 in pos: %d" % (motor3_pos)
     posefinal = delta_calcForward(motor1_pos, motor2_pos, motor3_pos)
     initializedMarkerArray = move_simulation_to_point(posefinal[1], posefinal[2], posefinal[3], initializedMarkerArray)
     point = generate_last_visited_point(posefinal[1], posefinal[2], posefinal[3])
-    initializedMarkerArray.markers.append(point)
+    current_trail_point_qtty += 1
+    if current_trail_point_qtty >= max_trail_points:
+        trailMarkerArray.markers.pop(0) # the 11th element is the first testpoint always
+        current_trail_point_qtty -= 1
+    trailMarkerArray.markers.append(point)
+    #publisher.publish(trailMarkerArray)
 
 def motors_listener():
     rospy.Subscriber('/motor1/state', JointState, callback_1)
@@ -117,5 +148,10 @@ while not rospy.is_shutdown():
     #initializedMarkerArray.markers.append(testpoint)
     # Publish the MarkerArray
     publisher.publish(initializedMarkerArray)
+    publisher.publish(trailMarkerArray)
+
+    #print "publishing %d number of markers on initializedMarkerArray" % (len(initializedMarkerArray.markers))
+    #print "publishing %d number of markers on trailMarkerArray" % (len(trailMarkerArray.markers))
+    print "number of points in current_trail_point: %d" %(current_trail_point)
     rospy.sleep(0.02)
        
